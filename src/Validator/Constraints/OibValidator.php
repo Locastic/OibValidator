47c54474c061e903799cd067e5868c29ab3d6f91
Duje Šarić <duje.saric@locastic.com>
2018-08-17 13:30:25 +0200

    Replaced DI with factory for oib content checker.

--- src/Validator/Constraints/OibValidator.php
+++ src/Validator/Constraints/OibValidator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Locastic\Component\OibValidator\Validator\Constraints;
 
+use Locastic\Component\OibValidator\Factory\OibContentCheckerFactory;
 use Locastic\Component\OibValidator\Utils\CheckDigitResolver;
 use Locastic\Component\OibValidator\Utils\OibContentCheckerInterface;
 use Symfony\Component\Validator\Constraint;
@@ -16,14 +17,14 @@ class OibValidator extends ConstraintValidator
      */
     private $contentChecker;
 
-    public function __construct(OibContentCheckerInterface $contentChecker)
+    public function __construct(OibContentCheckerInterface $contentChecker = null)
     {
         $this->contentChecker = $contentChecker;
     }
 
     public function validate($value, Constraint $constraint)
     {
-        if(!$this->contentChecker->hasOibValidLength($value)) {
+        if(!$this->getContentChecker()->hasOibValidLength($value)) {
             $this->context->buildViolation($constraint->lengthMessage)
                 ->addViolation();
             return;
@@ -39,4 +40,12 @@ class OibValidator extends ConstraintValidator
         }
     }
 
+    private function getContentChecker(): OibContentCheckerInterface
+    {
+        if (null === $this->contentChecker) {
+            $this->contentChecker = OibContentCheckerFactory::createOibContentChecker();
+        }
+
+        return $this->contentChecker;
+    }
 }
